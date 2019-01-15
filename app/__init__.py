a3074a12e57be2698322e9941dd3206521305d63
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-01-15 15:58:44 +0300

    [Chore #163222097]Create App configurations.

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+"""Creating an instance of app and create_app function."""
+# Third party import
+from flask import Flask
+
+# Local Import
+from instance.config import APP_CONFIG
+
+def create_app(config_name):
+    """Create App function."""
+    app = Flask(__name__, instance_relative_config=True)
+    
+    # Registering Blueprint
+    from .api.v2 import version2 as v2
+    app.register_blueprint(v1)
+
+    # Adding Configurations.
+    app.config.from_object(APP_CONFIG[config_name])
+    app.config.from_pyfile('config.py')
+
+
+    
+    return app
