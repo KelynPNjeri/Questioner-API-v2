1a2c4ed27033802e687f4c1c897e5901c5f8f9e7
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-01-17 11:33:56 +0300

    [Feature #163248553]Add create meetup function to meetup model.

--- app/api/v2/models/meetup_model.py
+++ app/api/v2/models/meetup_model.py
@@ -1,2 +1,12 @@
+from ....database import initialize_db
+
+db = initialize_db(config_name="development")
+cursor = db.cursor()
 class MeetupModel():
-    pass
+    """Deals with Meetup Operations."""
+    def create_meetup(self, data=None):
+        query = """
+        INSERT INTO meetups (location, images, topic, happening_on, description, tags) VALUES (%(location)s, %(images)s, %(topic)s, %(happening_on)s, %(description)s, %(tags)s);"""
+        cursor.execute(query, data)
+        db.commit()
+        return data
