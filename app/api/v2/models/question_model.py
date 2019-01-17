4cc684070f328697d0b3dbbce5778b0868751bd8
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-01-17 16:17:31 +0300

    [Feature #163248601]Create Questions Model.

--- app/api/v2/models/question_model.py
+++ app/api/v2/models/question_model.py
@@ -1,2 +1,13 @@
+from ....database import initialize_db
+
+db = initialize_db(config_name="development")
+cursor = db.cursor()
+
 class QuestionModel():
-    pass
+    """Deals with Questions Operations."""
+    def create_question(self, data=None):
+        query = """
+        INSERT INTO questions (created_by, meetup_id, title, body) VALUES (%(created_by)s, %(meetup_id)s, %(title)s, %(body)s);"""
+        cursor.execute(query, data)
+        db.commit()
+        return data
