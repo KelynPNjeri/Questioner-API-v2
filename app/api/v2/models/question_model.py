f175b1dea0a0d7177415223d67b571c2d851aec3
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-01-22 12:13:28 +0300

    [Feature #163248679]Add questions model for retrieving questions.

--- app/api/v2/models/question_model.py
+++ app/api/v2/models/question_model.py
@@ -1,13 +1,20 @@
+from psycopg2.extras import RealDictCursor
 from ....database import initialize_db
 
 db = initialize_db()
-cursor = db.cursor()
+cursor = db.cursor(cursor_factory=RealDictCursor)
 
 class QuestionModel():
     """Deals with Questions Operations."""
     def create_question(self, data=None):
         query = """
-        INSERT INTO questions (created_by, meetup_id, title, body) VALUES (%(created_by)s, %(meetup_id)s, %(title)s, %(body)s);"""
+        INSERT INTO questions (created_on, created_by, meetup_id, title, body) VALUES (%(created_on)s, %(created_by)s, %(meetup_id)s, %(title)s, %(body)s);"""
         cursor.execute(query, data)
         db.commit()
         return data
+
+    def retrieve_all_questions(self):
+        query = "SELECT * FROM questions;"
+        cursor.execute(query)
+        questions = cursor.fetchall()
+        return questions
