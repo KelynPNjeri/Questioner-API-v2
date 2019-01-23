cbb8c8efa8dfa73a4142927b9913b8a052acdc83
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-01-23 08:48:16 +0300

    [Feature #163348312]Add comments_api.

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -9,6 +9,7 @@ from .views.meetup_views import meetup_api
 from .views.question_views import question_api
 from .views.auth_views import auth_api
 from .views.rsvp_views import rsvp_api
+from .views.comment_views import comment_api
 
 version2 = Blueprint('Questioner version 2', __name__, url_prefix="/api/v2")
 
@@ -19,3 +20,4 @@ api.add_namespace(meetup_api, path="/meetups")
 api.add_namespace(question_api, path="/questions")
 api.add_namespace(auth_api, path="/auth")
 api.add_namespace(rsvp_api, path="/meetups/<int:meetup_id>/rsvps")
+api.add_namespace(comment_api, path="/comments")
